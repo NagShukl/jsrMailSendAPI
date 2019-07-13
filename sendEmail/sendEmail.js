20deb7b3ff856c15cc4a47ad1d473120538cf6ce
Nagendra <nagendra.shukla@gmail.com>
2019-07-13 12:48:36 +1000

    final validation

--- sendEmail/sendEmail.js
+++ sendEmail/sendEmail.js
@@ -14,24 +14,29 @@ class SendEmail {
         }
         req.on('data', (reqData) => {
             utils.log(`handleSendMailRequest: verifying request body.`);
-            const mailData = this.getRequestBody(reqData);
-            if (!mailData) {
+            const testData = require('../test/testData');
+            const mailData = this.getRequestBody(JSON.stringify(testData));//reqData);
+            if (!mailData.isValid) {
                 utils.log(`handleSendMailRequest: request body verification failed!`);
-                this.sendError(res, { httpError: constants.HTTP_BAD_REQUEST, appError: constants.APPLICATION_BAD_REQUEST });
+                const err = {
+                    errCode: '001',
+                    errMessage: '' + mailData.data
+                }
+                this.sendError(res, { httpError: constants.HTTP_BAD_REQUEST, appError: err });
                 return;
             }
             // Is a valid request, try to send
-            this.performMailSendAction(res, mailData);
+            this.performMailSendAction(res, mailData.data);
         });
     }
     performMailSendAction(res, mailData, providerNumber) {
         utils.log(`\n\n`);
-        if(!providerNumber) {
+        if (!providerNumber) {
             providerNumber = 0;
         }
         // Check If application has tried all provider then return.
         res.writeHead(200, constants.API_RESPONSE_CONTENT_TYPE);
-        if(providerNumber >= providers.length) {
+        if (providerNumber >= providers.length) {
             utils.log('performMailSendAction: Already tried with all configured providers. Unable to send Mail!');
             res.write(JSON.stringify(constants.API_ERROR_RESPONSE));
             res.end();
@@ -41,48 +46,11 @@ class SendEmail {
         const selectedProvider = providers[providerNumber].providerObject;
         const selectedProviderName = providers[providerNumber].name;
         utils.log(`performMailSendAction: Trying to send Mail with provider#${providerNumber} of ${providers.length}, name=${selectedProviderName}`);
-        mailData = {
-            from: {
-                    ame:'nagendra',
-                    email: 'nagendra.shukla@gmail.com'
-                },
-            to: [
-                {
-                    name:'nagendra',
-                    email: 'nagendra.shukla@gmail.com'
-                },
-                {
-                    name:'nagshukl',
-                    email: 'nagshukl@gmail.com'
-                }
-            ],
-            cc: [
-                {
-                    name:'nagendra',
-                    email: 'nagendra.shukla1@gmail.com'
-                },
-                {
-                    name:'nagshukl',
-                    email: 'nagshukl1@gmail.com'
-                }
-            ],
-            bcc: [
-                {
-                    name:'nagendra',
-                    email: 'nagendra.shukla2@gmail.com'
-                },
-                {
-                    name:'nagshukl',
-                    email: 'nagshukl2@gmail.com'
-                }
-            ],
-            subject: '111122233',
-            text: '123213131312312312313'
-        };
+
         selectedProvider.sendMail(mailData).then(data => {
             // Means one of provider, sent mail successfully.
             utils.log(`performMailSendAction: mail sent sucessfully using provider# ${providerNumber}, ${selectedProviderName}`);
-            
+
             res.write(JSON.stringify(constants.API_SUCCESS_RESPONSE));
             res.end();
             return true;
@@ -92,11 +60,107 @@ class SendEmail {
         });
     }
     getRequestBody(data) {
+        let err = [];
+        const res = {};
+        
         try {
-            return JSON.parse(data);
+            const mailData = JSON.parse(data);
+            let isValid = this.validateForNonBlankProperty(mailData, 'subject');
+            if (!isValid) {
+                err.push('Should have Subject');
+            }
+            isValid = this.validateForNonBlankProperty(mailData, 'text');
+            if (!isValid) {
+                err.push('Should have text');
+            }
+            err = this.validateFields(mailData, err);
+            if (err.length === 0) {
+                res.isValid = true;
+                res.data = mailData;
+
+            } else {
+                res.isValid = false;
+                res.data = err;
+            }
         } catch (err) {
-            return false;
+            res.isValid = false;
+            res.data = ['Invalid JSON!'];
         }
+        return res;
+    }
+    validateFields(mailData, err) {
+        const allEmailIds = [];
+        const fromError = this.validateFromField(mailData);
+        err = fromError.length > 0?[...err, fromError]:err;
+        const toError = this.validateToField(mailData, allEmailIds);
+        err = toError.length > 0?[...err, toError]:err;
+        const ccError = this.validateCCBCCField(mailData, 'cc', allEmailIds);
+        err = ccError.length > 0?[...err, ccError]:err;
+        const bccError = this.validateCCBCCField(mailData, 'bcc', allEmailIds);
+        err = bccError.length > 0?[...err, bccError]:err;
+        // finally validate for unique emailIds.
+        const duplicateError = this.validateUniqueEmails(allEmailIds);
+        err = duplicateError.length > 0?[...err, duplicateError]:err;
+        return err;
+    }
+    validateUniqueEmails(allEmailIds) {
+        const err = [];
+        var duplicates = allEmailIds.reduce(function (acc, el, i, arr) {
+            if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) acc.push(el); return acc;
+        }, []);
+        console.log('===>>>> '+duplicates.length+' : '+allEmailIds.length);
+        if (duplicates.length != 0) {
+            console.log('===>>>>AAA '+duplicates.length+' : '+allEmailIds.length);
+                err.push(duplicates+' Ids are duplicate in to, cc or bcc list.');
+        }
+        return err;
+    }
+    validateCCBCCField(mailData, propertyName, allEmailIds) {
+        const err = [];
+        const toList = mailData[propertyName];
+        toList.forEach(ele => {
+            allEmailIds.push(ele.email);
+            if (!this.isValidEmail(ele.email)) {
+                err.push(ele.email + ' is an Invalid email in ' + propertyName + ' list.');
+            }
+        });
+        return err;
+    }
+    validateToField(mailData, allEmailIds) {
+        const err = [];
+        if (!mailData.to) {
+            err.push('Should have "to" field.');
+        } else {
+            const toList = mailData.to;
+            toList.forEach(ele => {
+                allEmailIds.push(ele.email);
+                if (!this.isValidEmail(ele.email)) {
+                    err.push(ele.email + ' is an Invalid email in "to" field!');
+                }
+            });
+        }
+        return err;
+    }
+    validateFromField(mailData) {
+        const err = [];
+        if (!mailData.from) {
+            err.push('Should have from field.');
+        } else {
+            const from = mailData.from.email;
+            if (!this.isValidEmail(from)) {
+                err.push('Should have valid email in from field.');
+            }
+        }
+        return err;
+    }
+    isValidEmail(email) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return true;
+        }
+        return false;
+    }
+    validateForNonBlankProperty(mailData, propertyName) {
+        return (mailData[propertyName] && mailData[propertyName] !== '');
     }
     verifyRequestHeaders(req) {
         /**
---
+++ test/testData.js
@@ -0,0 +1,80 @@
+const validData = {
+    from: {
+            name:'nagendra',
+            email: 'nagendra.shukla@gmail.com'
+        },
+    to: [
+        {
+            name:'nagendra',
+            email: 'nagendra.shukla@gmail.com'
+        },
+        {
+            name:'nagshukl',
+            email: 'nagshukl2@gmail.com'
+        }
+    ],
+    cc: [
+        {
+            name:'nagendra',
+            email: 'nagendra.shukla13@gmail.com'
+        },
+        {
+            name:'nagshukl',
+            email: 'nagshukl14@gmail.com'
+        }
+    ],
+    bcc: [
+        {
+            name:'nagendra',
+            email: 'nagendra.shukla2@gmail.com'
+        },
+        {
+            name:'nagshukl',
+            email: 'nagshukl26@gmail.com'
+        }
+    ],
+    subject: '111122233',
+    text: '123213131312312312313'
+};
+const invalidData = {
+        from: {
+                name:'nagendra',
+                email: 'nagendra.shukla'
+            },
+        to: [
+            {
+                name:'nagendra',
+                email: 'nagendra.shukla@gmail.com'
+            },
+            {
+                name:'nagshukl',
+                email: 'nagshukl.gmail.com'
+            }
+        ],
+        cc: [
+            {
+                name:'nagendra',
+                email: 'nagendra.shukla1gmail.com'
+            },
+            {
+                name:'nagshukl',
+                email: 'nagshukl1@gmail.com'
+            }
+        ],
+        bcc: [
+            {
+                name:'nagendra',
+                email: 'nagendra.shukla@gmail.com'
+            },
+            {
+                name:'nagshukl',
+                email: 'nagshukl2gmail.com'
+            }
+        ],
+        subject: '111122233',
+        text: '123213131312312312313'
+    };
+module.exports = invalidData;
+
+// Uncomment below for invalid data.
+    
